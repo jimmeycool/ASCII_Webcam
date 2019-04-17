cc3841f60d136e98214dd48baeada42bd243dac4
mitch <mitch.keys1@gmail.com>
2019-04-17 14:26:36 -0700

    - basic terminal webcam made

--- ascii_webcam.py
+++ ascii_webcam.py
@@ -1,19 +1,42 @@
 import numpy as np
 import cv2
+import sys
+import os
 
 cap = cv2.VideoCapture(0)
 
 asciiScale =  "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+width = 270
+height = 90
+
+# print row by row the ascii array
+def printAscii(arr):
+    sys.stdout.flush()
+    for row in arr:
+        for elem in row:
+            sys.stdout.write(asciiScale[len(asciiScale) - elem -1])
+        sys.stdout.write("\n")
+    sys.stdout.write("\n")
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    bigGrey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.resize(bigGrey,(width, height))
+
+    # init ASCII array with normalized grey img
+    asciiArr = np.zeros((width, height))
+    asciiArr = cv2.normalize(gray,  asciiArr, 0, len(asciiScale)-1, cv2.NORM_MINMAX)
+
+    # clear terminal and print ascii img (only linix systems atm)
+    os.system("clear")
+    printAscii(asciiArr)
+
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    # cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
