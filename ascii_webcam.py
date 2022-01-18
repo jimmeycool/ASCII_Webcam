e3fbd30ed1d907ecdef0b464fa2f928f80dcc35a
jimmeycool <mitch.keys1@gmail.com>
2022-01-18 14:28:08 -0800

    Restructor the project to use more pythonic patterns

--- ascii_webcam.py
+++ ascii_webcam.py
@@ -1,45 +1,59 @@
-import numpy as np
-import cv2
-import sys
 import os
 
-cap = cv2.VideoCapture(0)
+import argparse
 
-asciiScale =  "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
-width = 270
-height = 90
+from webcam import Webcam, run
 
-# print row by row the ascii array
-def printAscii(arr):
-    sys.stdout.flush()
-    for row in arr:
-        for elem in row:
-            sys.stdout.write(asciiScale[len(asciiScale) - elem -1])
-        sys.stdout.write("\n")
-    sys.stdout.write("\n")
+parser = argparse.ArgumentParser(description='''
+    ASCII Console Webcam
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    Converts the terminal into a webcam display using only
+    ASCII charaters. You can specify the dimensions, which
+    camera to use, and how to clear the window.
+    ''')
 
-    # Our operations on the frame come here
-    bigGrey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.resize(bigGrey,(width, height))
+parser.add_argument('-w',
+                    '--width',
+                    type=int,
+                    default=80,
+                    help="The width in charaters of the screen")
+parser.add_argument('-H',
+                    '--height',
+                    type=int,
+                    default=30,
+                    help="The height in lines of the screen")
+parser.add_argument('-c',
+                    '--clear',
+                    type=str,
+                    default=None,
+                    help="Override the clear console command to specified")
+parser.add_argument(
+    '-d',
+    '--deviceid',
+    type=str,
+    default=0,
+    help="Camera id to use, 0 is system default, rest are other cameras")
 
-    # init ASCII array with normalized grey img
-    asciiArr = np.zeros((width, height))
-    asciiArr = cv2.normalize(gray,  asciiArr, 0, len(asciiScale)-1, cv2.NORM_MINMAX)
-
-    # clear terminal and print ascii img (only linix systems atm)
-    os.system("clear")
-    printAscii(asciiArr)
+args = parser.parse_args()
 
 
-    # Display the resulting frame
-    # cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def _get_clear() -> str:
+    """
+    Gets the clear command for the printer
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    Returns:
+        str: clear command
+    """
+    return args.clear or 'cls' if os.name == 'nt' else 'clear'
+
+
+def main():
+    """
+    Process the args and run the webcam
+    """
+    with Webcam(args.deviceid).start() as cam:
+        run(cam, args.width, args.height, _get_clear())
+
+
+if __name__ == '__main__':
+    main()
